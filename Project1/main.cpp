ebd53b09109dc3a4c7f78124e2fc09dbab325ad7
Linker <shil33960@gmail.com>
2025-05-06 13:25:46 +0800

    modify

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 int main()
 {
-	std::string mark = "Hello World" ;
-	std::cout << mark << std::endl;
-	int a = 3;
-	int s = 2;
-	double x = 2.12221;
-	std::cout << x;
+	std::cout << "**************计算开始**************\n";
+	int number1, number2;
+	char op;
+	std::cout << "enter + - * /" << std::endl;
+	std::cin >> op;
+	std::cout << "enter number1: ";
+	std::cin >> number1;
+	std::cout << "enter number2: ";
+	std::cin >> number2;
+	switch (op) {
+		
+	case '+':
+		std::cout << number1 << " + " << number2 << " = " << number1 + number2;
+		break;
+	case '-':
+		std::cout << number1 << " - " << number2 << " = " << number1 - number2;
+		break;
+	case '*':
+		std::cout << number1 << " × " << number2 << " = " << number1 * number2;
+		break;
+	case'/':
+		std::cout << number1 << " ÷ " << number2 << " = " << number1 / number2;
+		break;
+	default:
+		std::cout << "That wasn't a valid response" << std::endl;
+	}
+	std::cout << "\n********************结束************************";
 	return 0;
 }
