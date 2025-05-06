3e1da91d5fbfdd9e4890548f321e4a6fe9e637f9
Linker <shil33960@gmail.com>
2025-05-07 00:01:05 +0800

    new file:   practise/p7.c

---
+++ practise/p7.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct arcNode{
+    int adjvex;
+    struct arcNode* nextarc;
+}arcNode;
+typedef struct Vnode{
+    char V;
+    arcNode* firstarc;
+}Vnode;
+typedef struct Graph{
+    int vertices, edges;
+    Vnode** adjList;
+}Graph;
+
+Graph* createGraph(int vertices, int edges)
+{
+    Graph* G = malloc(sizeof(Graph));
+    G->adjList = malloc(sizeof(Vnode) * vertices);
+    G->vertices = vertices;
+    G->edges = edges;
+    return G;
+}
+void createADJlise(Graph* G)
+{
+    char a[] = "abcdefg";
+    for (int i = 0; i < G->vertices; i++) {
+        G->adjList[i] = malloc(sizeof(Vnode));
+        G->adjList[i]->V = a[i];
+        G->adjList[i]->firstarc = NULL;
+    }
+    
+}
+int Loc(Graph* G, char e)
+{
+    for (int i = 0; i < G->vertices; i++) {
+       if (e == G->adjList[i]->V)
+            return i;
+    }
+    return -1;
+}
+arcNode* createARCnode(int e)
+{
+    arcNode* newNode = malloc(sizeof(arcNode));
+    newNode->adjvex = e;
+    newNode->nextarc = NULL;
+    return newNode;
+}
+void createEdge(Graph* G, int i, int j)
+{
+    
+    arcNode* newNode = createARCnode(j);
+    newNode->nextarc = G->adjList[i]->firstarc;
+    G->adjList[i]->firstarc = newNode;
+
+    newNode = createARCnode(i);
+    newNode->nextarc = G->adjList[j]->firstarc;
+    G->adjList[j]->firstarc = newNode;
+    printf("%c--%c complished\n", G->adjList[i]->V, G->adjList[j]->V);
+    
+}
+void run(Graph* G)
+{
+    for (int t = 0; t < G->edges; t++) {
+        char v1, v2;
+        printf("Enter vertices: ");
+        scanf(" %c%c", &v1, &v2);
+        int i = Loc(G, v1);
+        int j = Loc(G, v2);
+        createEdge(G, i, j);
+    }
+}
+
+int main()
+{
+    Graph* G = createGraph(5, 7);
+    createADJlise(G);
+    run(G);
+    return 0;
+}
