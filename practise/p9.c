6c7208fb0864e3597612992cd0123efdd817e382
Linker <shil33960@gmail.com>
2025-05-18 20:33:58 +0800

    modified:   practise/p8.c 	new file:   practise/p9.c

--- practise/p9.c
+++ practise/p9.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
-/*图的DFS遍历*/
+#define Max 100
 typedef struct node{
     int vertex;
     struct node* next;
 }node;
-typedef struct graph{
-    int numVertex;
-    node* adjList[100];
-    bool visited[100];
+typedef struct Graph{
+    int numV;
+    node* adjList[Max];
+    bool visited[Max];
 }G;
+
 G* createGraph(int vertices)
 {
     G* graph = malloc(sizeof(G));
-    graph->numVertex = vertices;
-    for (int i = 0; i < vertices; i++){
+    graph->numV = vertices;
+    for (int i = 0; i < vertices; i++) {
         graph->adjList[i] = NULL;
         graph->visited[i] = false;
     }
@@ -28,37 +29,39 @@ node* createNode(int e)
     newNode->next = NULL;
     return newNode;
 }
-void addEdge(G* graph, int sre, int dest)
+void Edge(G* graph, int sre, int dest)
 {
-    node* temp = createNode(dest);
-    temp->next = graph->adjList[sre];
-    graph->adjList[sre] = temp;
+    node* newNode = createNode(dest);
+    newNode->next = graph->adjList[sre];
+    graph->adjList[sre] = newNode;
 
-    temp = createNode(sre);
-    temp->next = graph->adjList[dest];
-    graph->adjList[dest] = temp;
+    newNode = createNode(sre);
+    newNode->next = graph->adjList[dest];
+    graph->adjList[dest] = newNode;
+    
 
 }
-void DFS(G* graph, int vertex) {
+void DFS(G* graph, int vertex)
+{
     graph->visited[vertex] = true;
     printf("%d ", vertex);
 
     node* temp = graph->adjList[vertex];
     while (temp) {
-        int a = temp->vertex;
-        if (!graph->visited[a]) {
-            DFS(graph, a);
+        if (!graph->visited[temp->vertex]) {
+            DFS(graph, temp->vertex);
         }
         temp = temp->next;
     }
+    
 }
-int main() {
-
+int main()
+{
     G* graph = createGraph(5);
-    addEdge(graph, 0, 1);
-    addEdge(graph, 0, 2);
-    addEdge(graph, 1, 3);
-    addEdge(graph, 1, 4);
+    Edge(graph, 0, 1);
+    Edge(graph, 0, 2);
+    Edge(graph, 1, 3);
+    Edge(graph, 1, 4);
 
     printf("DFS traversal: ");
     DFS(graph, 0); // 从顶点0开始    
