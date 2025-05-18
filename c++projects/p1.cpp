6afc1d01dd23e3658e5ffa20e883c104355c3b29
Linker <shil33960@gmail.com>
2025-05-18 23:51:19 +0800

    modified:   c++projects/p1.cpp

--- c++projects/p1.cpp
+++ c++projects/p1.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+/*创建命名空间*/
+namespace first {
+	int x = 1;
+	int y = 1;
+	int t = 2;
+}
 int main()
 {
-    int a = 2;
-    std::cout << "a = " << a << std::endl;
-    std::cout << "hello!";
-    return 0;
+	using namespace first;
+	std::cout << t + x;
+	return 0;
 }
